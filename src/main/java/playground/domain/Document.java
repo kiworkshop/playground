15f68c346fbfe14ce47214aae535307d0a1aa7df
chunghyeon-kim <asdddq11@gmail.com>
2021-10-04 18:33:48 +0900

    feat: Add methods to domain classes have tests success

--- src/main/java/playground/domain/Document.java
+++ src/main/java/playground/domain/Document.java
@@ -7,6 +7,7 @@ import playground.constants.Category;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Builder
 @Getter
@@ -24,7 +25,7 @@ public class Document {
         documentApprovals = new ArrayList<>();
         approvalState = ApprovalState.DRAFTING;
 
-        for(User approver : approvers) {
+        for (User approver : approvers) {
             DocumentApproval documentApproval = DocumentApproval.builder()
                     .approver(approver)
                     .approvalState(ApprovalState.DRAFTING)
@@ -36,6 +37,44 @@ public class Document {
     }
 
     public void approveBy(User approver, String approvalComment) {
-        
+        approverCheck(approver);
+        for (int i = 0; i < documentApprovals.size(); i++) {
+            if (documentApprovals.get(i).getApprover().equals(approver)) {
+                checkPriorApprovals(i);
+                documentApprovals.get(i).setApprovalComment(approvalComment);
+                if (approvalComment.equals("결재 승인합니다.") || approvalComment.equals("확인했습니다.")) {
+                    documentApprovals.get(i).setApprovalState(ApprovalState.APPROVED);
+                }
+            }
+        }
+        checkAllApprovals();
+
+    }
+
+    private void checkAllApprovals() {
+        for (DocumentApproval documentApproval : documentApprovals) {
+            if (documentApproval.getApprovalState() != ApprovalState.APPROVED) {
+                return;
+            }
+        }
+        approvalState = ApprovalState.APPROVED;
+    }
+
+    private void checkPriorApprovals(int approvalIndex) {
+        for (int i=0; i<approvalIndex; i++) {
+            if(documentApprovals.get(i).getApprovalState() != ApprovalState.APPROVED) {
+                throw new IllegalArgumentException("우선순위 결재가 아직 처리되지 않았습니다.");
+            }
+        }
+    }
+
+    private void approverCheck(User approver) {
+        Optional<DocumentApproval> matchApproval = documentApprovals.stream()
+                .filter(approval -> approval.getApprover() == approver)
+                .findAny();
+
+        if(matchApproval.isEmpty()) {
+            throw new IllegalArgumentException("결재 권한이 없습니다.");
+        }
     }
 }
--- src/main/java/playground/domain/DocumentApproval.java
+++ src/main/java/playground/domain/DocumentApproval.java
@@ -2,11 +2,13 @@ package playground.domain;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 import playground.constants.ApprovalState;
 
 @Builder
 @Getter
-public class    DocumentApproval {
+@Setter
+public class DocumentApproval {
 
     User approver;
     String approvalComment;
