333ed2011dcfd42dc48f9bcc99f05789c6bd9f20
Beenie93 <seongbeen93@naver.com>
2021-10-08 03:58:33 +0900

    feat : 승인 상태인지 확인하는 기능 및 테스트 구현

---
+++ src/test/java/learning/ApprovalStateTest.java
@@ -0,0 +1,21 @@
+package learning;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ApprovalStateTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"DRAFTING, false", "CANCELED, false", "APPROVED, true"})
+    @DisplayName("승인 상태인지 확인한다.")
+    void isApproved(ApprovalState approvalState, boolean expected) {
+        //when
+        boolean isApproved = approvalState.isApproved();
+
+        //then
+        assertThat(isApproved).isEqualTo(expected);
+    }
+}
