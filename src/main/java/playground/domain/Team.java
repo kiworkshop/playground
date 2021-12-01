859b9abb64fd92b0375b601bf9a8ff8d22f7f8cd
daeun_lee <daeun_lee@fursys.com>
2021-12-01 18:01:27 +0900

    feat: 팀 전체 조회 api 추가

--- src/main/java/playground/domain/Team.java
+++ src/main/java/playground/domain/Team.java
@@ -1,6 +1,7 @@
 package playground.domain;
 
-import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,11 +9,13 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-@AllArgsConstructor
+@Getter
+@NoArgsConstructor
 public class Team {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
+
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,6 @@ insert into user(id, name) values (2, '이다은');
 
 --insert into document(id, title, category, contents, drafter_id, approval_state)
 --    values (1, '첫번째 문서제목', 'EDUCATION', '문서내용', 1, 'DRAFTING');
+
+insert into team(id, name) values (1, '정산시스템팀');
+insert into team(id, name) values (2, '서비스개발팀');
