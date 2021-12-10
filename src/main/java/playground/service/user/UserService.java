b8a6e758d49bb46e84138d4d7f220c53d01636bf
Travis <travis@techtaka.com>
2021-12-11 00:06:38 +0900

    feat: TeamRepository 추가와 UserService에 조회 method들 추가

--- src/main/java/playground/service/user/UserService.java
+++ src/main/java/playground/service/user/UserService.java
@@ -3,6 +3,8 @@ package playground.service.user;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import playground.domain.user.Team;
+import playground.domain.user.TeamRepository;
 import playground.domain.user.User;
 import playground.domain.user.UserRepository;
 
@@ -15,12 +17,26 @@ import java.util.Optional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final TeamRepository teamRepository;
 
     public Optional<User> findById(Long userId) {
         return userRepository.findById(userId);
     }
 
+    public User getById(Long userId) {
+        return findById(userId)
+            .orElseThrow(() -> new IllegalArgumentException(String.format("존재하지 않는 사용자입니다. userId = %s", userId)));
+    }
+
     public List<User> findAllById(List<Long> userIds) {
         return userRepository.findAllById(userIds);
+    }
+
+    public Optional<Team> findTeamById(Long teamId) {
+        return teamRepository.findById(teamId);
+    }
+
+    public List<Team> findAllTeams() {
+        return teamRepository.findAll();
     }
 }
